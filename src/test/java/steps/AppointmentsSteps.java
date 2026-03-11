f0bde4c1118bae6b7f97e76b7318dbe8e90d7a50
Anna Levin <alevinskaia38541@ucumberlands.edu>
2026-03-11 10:37:16 -0500

    appointment page

--- src/test/java/steps/AppointmentsSteps.java
+++ src/test/java/steps/AppointmentsSteps.java
@@ -1,4 +1,79 @@
 package steps;
 
-public class AppointmentsSteps {
+import com.github.javafaker.Faker;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.bytebuddy.implementation.attribute.AnnotationAppender;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pages.AppointmentsPage;
+import pages.CommonPages;
+import pages.LoginPage;
+import utils.BaseUI;
+import utils.Driver;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class AppointmentsSteps extends BaseUI {
+
+    WebDriver driver = Driver.getDriver();
+    LoginPage loginPage = new LoginPage();
+    AppointmentsPage appointmentsPage = new AppointmentsPage();
+    CommonPages commonPages = new CommonPages();
+    Faker faker = new Faker();
+
+
+    @When("doctor navigates to the appointment page")
+    public void doctor_navigates_to_the_appointment_page() {
+        waitUntilVisible(20, commonPages.appointments);
+        waitAndClick(commonPages.appointments);
+        Assertions.assertTrue(appointmentsPage.appointmentsSign.isDisplayed());
+    }
+
+    @When("clicks on the schedule appointment button")
+    public void clicks_on_the_schedule_appointment_button() {
+        waitAndClick(appointmentsPage.scheduleAppointmentButton);
+    }
+
+    @When("filling out the form")
+    public void filling_out_the_form() throws InterruptedException {
+        waitUntilVisible(40, appointmentsPage.patientDropDown);
+        appointmentsPage.patientDropDown.click();
+        appointmentsPage.getPatientByName("Anderson", "Lisa").click();
+
+        appointmentsPage.providerDropDown.click();
+        appointmentsPage.getProvider("Osei").click();
+
+
+        appointmentsPage.dateField.click();
+        String randomDate = new SimpleDateFormat("MM/dd/yyyy")
+                .format(faker.date().future(30, TimeUnit.DAYS));
+        appointmentsPage.dateField.sendKeys(randomDate);
+
+        appointmentsPage.startTimeField.click();
+        appointmentsPage.startTimeField.sendKeys("10:00");
+        appointmentsPage.startTimeField.sendKeys(Keys.ARROW_UP);
+
+        appointmentsPage.startTimeField.click();
+
+        appointmentsPage.endTimeField.sendKeys("11:00");
+        appointmentsPage.endTimeField.sendKeys(Keys.ARROW_DOWN);
+        appointmentsPage.endTimeField.click();
+    }
+
+    @When("clicks on the schedule button")
+    public void clicks_on_the_schedule_button(){
+        waitAndClick(appointmentsPage.scheduleButton);
+    }
+
+    @Then("doctor should see appointment scheduled")
+    public void doctor_should_see_appointment_scheduled() throws InterruptedException {
+        //Thread.sleep(2000);
+        waitUntilVisible(appointmentsPage.appointmentScheduledSuccessfullyMessageBy);
+        Assertions.assertTrue(appointmentsPage.appointmentScheduledSuccessfullyMessage.isDisplayed());
+    }
 }
---
+++ src/test/resources/features/Appointments.feature
@@ -0,0 +1,14 @@
+Feature: Appointment page functionality
+
+  Background: doctor sign in
+    Given doctor goes to sign in page
+    When doctor enters email "dr.chen@mediflow.com"
+    And doctor enters password "Test@1234"
+    And doctor clicks on sign in button
+
+  Scenario: doctor should be able to schedule the appointment
+    When doctor navigates to the appointment page
+    And clicks on the schedule appointment button
+    And filling out the form
+    And clicks on the schedule button
+    Then doctor should see appointment scheduled
