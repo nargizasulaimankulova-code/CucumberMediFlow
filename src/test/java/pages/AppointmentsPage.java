9326f7f48c25b17b7ef7636f925301a7806ff175
Anna Levin <alevinskaia38541@ucumberlands.edu>
2026-03-11 10:15:55 -0500

    appointment page feature file

--- src/test/java/pages/AppointmentsPage.java
+++ src/test/java/pages/AppointmentsPage.java
@@ -1,4 +1,64 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Driver;
+
+import java.time.Duration;
+
 public class AppointmentsPage {
+
+    public AppointmentsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//h1[text()='Appointments']")
+    public WebElement appointmentsSign;
+
+    @FindBy(xpath = "//button[@data-testid='schedule-appointment-btn']")
+    public WebElement scheduleAppointmentButton;
+
+    @FindBy(xpath = "//label[text()='Patient *']/following::button[@role='combobox'][1]")
+    public WebElement patientDropDown;
+
+    public WebElement getPatientByName(String firstName, String lastName) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        By patientLocator = By.xpath(
+                "//div[@role='option' and contains(.,'" + firstName + "') and contains(.,'" + lastName + "')]"
+        );
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(patientLocator));
+    }
+
+    public WebElement getProvider(String provider) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        By providerLocator = By.xpath(
+                "//div[@role='option' and contains(.,'" + provider + "')]"
+        );
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(providerLocator));
+    }
+
+    @FindBy(xpath = "//label[text()='Provider *']/following::button[@role='combobox'][1]")
+    public WebElement providerDropDown;
+
+
+    @FindBy(xpath = "//label[normalize-space()='Date *']/parent::div/input")
+    public WebElement dateField;
+
+    @FindBy(xpath = "//label[text()='Start Time *']/following::input[@type='time'][1]")
+    public WebElement startTimeField;
+
+    @FindBy(xpath = "//label[text()='Start Time *']/following::input[@type='time'][2]")
+    public WebElement endTimeField;
+
+    @FindBy(xpath = "//button[text()='Schedule']")
+    public WebElement scheduleButton;
+
+    @FindBy(xpath = "//div[contains(text(),'Appointment scheduled successfully')]")
+    public WebElement appointmentScheduledSuccessfullyMessage;
+
+    public By appointmentScheduledSuccessfullyMessageBy = By.xpath("//li[@role='status']//*[contains(.,'Appointment scheduled successfully')]");
 }
--- src/test/java/steps/AppointmentsSteps.java
+++ src/test/java/steps/AppointmentsSteps.java
@@ -1,4 +1,89 @@
 package steps;
 
-public class AppointmentsSteps {
+import com.github.javafaker.Faker;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.bytebuddy.implementation.attribute.AnnotationAppender;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pages.AppointmentsPage;
+import pages.CommonPages;
+import pages.LoginPage;
+import utils.BaseUI;
+import utils.Driver;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class AppointmentsSteps extends BaseUI {
+
+    WebDriver driver = Driver.getDriver();
+    LoginPage loginPage = new LoginPage();
+    AppointmentsPage appointmentsPage = new AppointmentsPage();
+    CommonPages commonPages = new CommonPages();
+    Faker faker = new Faker();
+
+
+    @When("doctor navigates to the appointment page")
+    public void doctor_navigates_to_the_appointment_page() {
+        waitUntilVisible(20, commonPages.appointments);
+        waitAndClick(commonPages.appointments);
+        Assertions.assertTrue(appointmentsPage.appointmentsSign.isDisplayed());
+    }
+
+    @When("clicks on the schedule appointment button")
+    public void clicks_on_the_schedule_appointment_button() {
+        waitAndClick(appointmentsPage.scheduleAppointmentButton);
+    }
+
+    @When("filling out the form")
+    public void filling_out_the_form() throws InterruptedException {
+        waitUntilVisible(40, appointmentsPage.patientDropDown);
+        appointmentsPage.patientDropDown.click();
+        appointmentsPage.getPatientByName("Anderson", "Raj").click();
+
+        appointmentsPage.providerDropDown.click();
+        appointmentsPage.getProvider("Osei").click();
+
+
+        appointmentsPage.dateField.click();
+        String randomDate = new SimpleDateFormat("MM/dd/yyyy")
+                .format(faker.date().future(30, TimeUnit.DAYS));
+        appointmentsPage.dateField.sendKeys(randomDate);
+
+        appointmentsPage.startTimeField.click();
+        appointmentsPage.startTimeField.sendKeys("10:00");
+        appointmentsPage.startTimeField.sendKeys(Keys.ARROW_UP);
+        //appointmentsPage.startTimeField.sendKeys(Keys.ARROW_UP);
+        appointmentsPage.startTimeField.click();
+        //Thread.sleep(2000);
+        appointmentsPage.endTimeField.sendKeys("11:00");
+        appointmentsPage.endTimeField.sendKeys(Keys.ARROW_DOWN);
+        appointmentsPage.endTimeField.click();
+        //Thread.sleep(2000);
+
+       // appointmentsPage.scheduleButton.click();
+        //Thread.sleep(2000);
+
+    }
+
+    @When("clicks on the schedule button")
+    public void clicks_on_the_schedule_button() throws InterruptedException {
+        //Thread.sleep(2000);
+        waitAndClick(appointmentsPage.scheduleButton);
+        //appointmentsPage.scheduleButton.click();
+    }
+
+    @Then("doctor should see appointment scheduled")
+    public void doctor_should_see_appointment_scheduled() throws InterruptedException {
+        Thread.sleep(2000);
+        waitUntilVisible(appointmentsPage.appointmentScheduledSuccessfullyMessageBy);
+        //waitUntilVisible(20, appointmentsPage.appointmentScheduledSuccessfullyMessage);
+        Assertions.assertTrue(appointmentsPage.appointmentScheduledSuccessfullyMessage.isDisplayed());
+    }
+
+
 }
