7d70ace3409bb85040371da5af399bf6e14e4d54
Angelina <angelina@Angelinas-MacBook-Air.local>
2026-03-10 19:36:44 -0500

    Balnur and Angelina Schedule

--- src/test/java/steps/ScheduleSteps.java
+++ src/test/java/steps/ScheduleSteps.java
@@ -1,4 +1,48 @@
 package steps;
 
-public class ScheduleSteps {
-}
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import pages.SchedulePage;
+import utils.BaseUI;
+
+public class ScheduleSteps extends BaseUI {
+        SchedulePage schedulePage = new SchedulePage();
+
+        @When("user opens My Schedule page")
+        public void user_opens_my_schedule_page() {
+            waitUntilVisible(10, schedulePage.myScheduleTab);
+            schedulePage.myScheduleTab.click();
+            waitUntilVisible(10, schedulePage.myScheduleSign);
+        }
+
+        @Then("user should see My Schedule page elements")
+        public void user_should_see_my_schedule_page_elements() {
+            Assertions.assertTrue(schedulePage.myScheduleSign.isDisplayed());
+            Assertions.assertTrue(schedulePage.todayDropdown.isDisplayed());
+        }
+
+        @When("user clicks Check In button")
+        public void user_clicks_check_in_button() {
+            schedulePage.clickCheckIn();
+        }
+
+        @Then("appointment status should change to Checked In")
+        public void appointment_status_should_change_to_checked_in() {
+            schedulePage.verifyCheckedInStatus();
+        }
+
+        @When("user clicks Complete button")
+        public void user_clicks_complete_button() {
+            schedulePage.clickComplete();
+        }
+
+        @Then("appointment status should change to Completed")
+        public void appointment_status_should_change_to_completed() {
+            waitUntilVisible(10,schedulePage.completedPatientCount);
+            Assertions.assertTrue(schedulePage.completedPatientCount.isDisplayed());
+        }
+    }
+
+
---
+++ src/test/resources/features/Schedule.feature
@@ -0,0 +1,21 @@
+Feature: schedule page related test cases
+
+  Background:
+    Given user goes to sign in page
+    And user enters username "dr.chen@mediflow.com"
+    And user enters password "Test@1234"
+    And user clicks on sign in button
+
+  Scenario: User verifies My Schedule page elements
+    When user opens My Schedule page
+    Then user should see My Schedule page elements
+
+  Scenario: User checks in appointment
+    When user opens My Schedule page
+    And user clicks Check In button
+    Then appointment status should change to Checked In
+
+  Scenario: User completes appointment
+    When user opens My Schedule page
+    And user clicks Complete button
+    Then appointment status should change to Completed
