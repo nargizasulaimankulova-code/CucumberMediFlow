2c534a8688387a21a9cf6921eead3734aad75f0b
Anna Levin <alevinskaia38541@ucumberlands.edu>
2026-03-25 18:46:50 -0500

    patient api added

--- src/test/java/steps/api/PatientAPISteps.java
+++ src/test/java/steps/api/PatientAPISteps.java
@@ -27,6 +27,13 @@ public class PatientAPISteps extends ApiUtils {
     public void user_has_valid_authorization(String userType) {
         request = request.header("Authorization", "Bearer " + getToken(userType));
     }
+
+    @Given("{string} has invalid authorization")
+    public void has_invalid_authorization(String userType) {
+        request = request.header("Authorization", getToken(userType));
+    }
+
+
     @When("user hits POST {string}")
     public void user_hits_post(String endPoint) {
         Faker faker = new Faker();
@@ -45,10 +52,31 @@ public class PatientAPISteps extends ApiUtils {
         patientRequest.setInsurance_provider("Aetna");
         patientRequest.setInsurance_policy_number("POL12345");
 
+        response = request.body(patientRequest).post(endPoint);
+    }
 
+    @When("user hits POST {string} with missing required filed")
+    public void user_hits_post_with_missing_required_filed(String endPoint) {
+        Faker faker = new Faker();
+
+        CreatePatientRequest patientRequest = new CreatePatientRequest();
+        patientRequest.setFirst_name(faker.name().firstName());
+        patientRequest.setLast_name(faker.name().lastName());
+
+        patientRequest.setDob("1980-03-24");
+        patientRequest.setGender("Male");
+        //patientRequest.setPhone("3230000099");
+        //patientRequest.setEmail("candy99@gmail.com");
+        patientRequest.setAddress("1 candy st");
+        patientRequest.setEmergency_contact_name("John");
+        patientRequest.setEmergency_contact_phone("1234567890");
+        patientRequest.setInsurance_provider("Aetna");
+        patientRequest.setInsurance_policy_number("POL12345");
 
         response = request.body(patientRequest).post(endPoint);
     }
+
+
     @Then("verify status code is {int}")
     public void verify_status_code_is(Integer statusCode) {
         int actualStatusCode = response.statusCode();
--- src/test/resources/features/api/Patients.feature
+++ src/test/resources/features/api/Patients.feature
@@ -4,7 +4,7 @@ Feature: test patient creation
 
     #using pojo
   @createPatient
-Scenario Outline: verify user can create patient
+Scenario Outline: verify user can create patient 201
   Given "<userType>" has valid authorization
   When user hits POST "/api-patients"
   Then verify status code is 201
@@ -13,3 +13,25 @@ Scenario Outline: verify user can create patient
     | doctor         |
     | nurse          |
     | office manager |
+
+  @createPatient
+  Scenario Outline: verify user cannot create patient with missing required filed 400
+    Given "<userType>" has valid authorization
+    When user hits POST "/api-patients" with missing required filed
+    Then verify status code is 400
+    Examples:
+      | userType       |
+      | doctor         |
+      | nurse          |
+      | office manager |
+
+  @createPatient
+  Scenario Outline: verify user cannot create patient with invalid authorization 401
+    Given "<userType>" has invalid authorization
+    When user hits POST "/api-patients"
+    Then verify status code is 401
+    Examples:
+      | userType       |
+      | doctor         |
+      | nurse          |
+      | office manager |
